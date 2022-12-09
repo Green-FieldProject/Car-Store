df4b3fd128a0871284a742f7cd97db5ffc744875
yahyadhaou <yahya.dhaou75@gmail.com>
2022-12-09 12:46:02 +0100

    done

--- client/src/component/OnePost.jsx
+++ client/src/component/OnePost.jsx
@@ -13,6 +13,16 @@ export default function OnePost({ element }) {
 }
 //todo akraw louled chnya aammlet ahwka aamlet  delete wel update wel updatte bch taamloha bel price 
 // const [price,setPrice] = useState('') lahne bch naamlo update lel price  w el function delet ahyka hadhra 
+// var update=(id)=>{
+  //   axios.put(`http://localhost:4000/api/car/:id${id}`,{price})
+  //   .then((res)=>{
+  //     console.log("hello")
+  //   })
+  //   .catch(err=>{console.log(err)
+  // })
+  // window.location.reload(false)
+  
+  // }
 // var deleted=(id)=>{
   //   axios.delete(`http://localhost:4000/api/car/:id${id}`)
   //   .then((res)=>{
@@ -22,16 +32,7 @@ export default function OnePost({ element }) {
   // })
   // window.location.reload(false)
   // }
-  // var update=(id)=>{
-  //   axios.put(`http://localhost:4000/api/car/:id${id}`,{price})
-  //   .then((res)=>{
-  //     console.log("hello")
-  //   })
-  //   .catch(err=>{console.log(err)
-  // })
-  // window.location.reload(false)
-  
-  // } 
+  // 
   // <input type="text" className="add" onChange={(e)=>setPrice(e.target.value)}/> <button className="button" onClick={()=> update(element._id)}>update</button>
   //       <button className="button" onClick={()=>deleted(element._id)}>delete</button>
   
